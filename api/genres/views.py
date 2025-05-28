32047b6a18248c9fc85ac9745e864088650a7050
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-05-28 17:37:27 -0300

    updating to rest framework

--- api/genres/views.py
+++ api/genres/views.py
@@ -1,30 +1,14 @@
-import json
-
-from django.http import HttpRequest, JsonResponse
-from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
 
 from genres.models import Genre
+from genres.serializers import GenreSerializer
 
 
-@csrf_exempt
-def genre_create_list_view(request: HttpRequest) -> JsonResponse:
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
+class GenreListCreateView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
-    if request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data['name'])
-        new_genre.save()
-        return JsonResponse(
-            {'message': f'id {new_genre.id} - {new_genre.name}'}, status=201
-        )
 
-@csrf_exempt
-def genre_detail_view(request: HttpRequest, pk: int) -> JsonResponse:
-    genre = get_object_or_404(Genre, pk=pk)
-
-    data = {'id': genre.id, 'name': genre.name}
-    return JsonResponse(data)
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
